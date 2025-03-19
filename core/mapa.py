146c0901a1d74750855abaa7958ea9b30c3f81e1
Jose Francisco Hurtado Valero <jfhv1@alu.ua.es>
2025-03-19 11:42:29 +0100

    Búsqueda de vecinos en el mapa
    Ya está desarrollado el método de búsqueda de vecinos en el mapa, para saber si dos regiones están o no conectadas entre sí.
    
    Ofrece la posibilidad de incluir o no las conexiones de las diagonales, por defecto están puestas.

--- core/mapa.py
+++ core/mapa.py
@@ -32,13 +32,80 @@ class Mapa:
                     #if (fila, columna) == (reino_1 or reino_2):
                     #    pass"""
 
-    def crear_conexiones(self)-> List[tuple]:
+    def crear_nodos(self)-> List[tuple]:
 
-        mapa_conexiones = []
+        mapa_nodos = []
 
         for fila in range(self.filas):
             for columna in range(self.columnas):
-                mapa_conexiones.append((fila, columna))
+                mapa_nodos.append((fila, columna))
 
-        return mapa_conexiones
+        return mapa_nodos
+
+
+    def crear_aristas(self, nodos: List[tuple], diagonales: bool = True)-> List[list[tuple]]:
+
+        aristas = [] # Listado de aristas para cada nodo
+        cont = 0
+
+        if diagonales:
+            for nodo_diagonal in nodos:
+
+                aristas.append([]) # Anydimos una lista vacia para cada nodo
+
+                ### HORIZONTALES Y VERTICALES ###
+                if 0 <= nodo_diagonal[0] - 1:                                                           # Fila arriba
+                    aristas[cont].append((nodo_diagonal[0] - 1, nodo_diagonal[1]))
+                if nodo_diagonal[0] + 1 < self.filas:                                                   # Fila abajo
+                    aristas[cont].append((nodo_diagonal[0] + 1, nodo_diagonal[1]))
+                if 0 <= nodo_diagonal[1] - 1:                                                           # Columna izquierda
+                    aristas[cont].append((nodo_diagonal[0], nodo_diagonal[1] - 1))
+                if nodo_diagonal[1] + 1 < self.columnas:                                                # Columna derecha
+                    aristas[cont].append((nodo_diagonal[0], nodo_diagonal[1] + 1))
+
+                ### DIAGONALES ###
+                if (0 <= nodo_diagonal[0] - 1) and (0 <= nodo_diagonal[1] - 1):                         # Diagonal arriba izquierda
+                    aristas[cont].append((nodo_diagonal[0] - 1, nodo_diagonal[1] - 1))
+                if (0 <= nodo_diagonal[0] - 1) and (nodo_diagonal[1] + 1 < self.columnas):              # Diagonal arriba derecha
+                    aristas[cont].append((nodo_diagonal[0] - 1, nodo_diagonal[1] + 1))
+                if (nodo_diagonal[0] + 1 < self.filas) and (0 <= nodo_diagonal[1] - 1):                 # Diagonal abajo izquierda
+                    aristas[cont].append((nodo_diagonal[0] + 1, nodo_diagonal[1] - 1))
+                if (nodo_diagonal[0] + 1 < self.columnas) and (nodo_diagonal[1] + 1 < self.columnas):   # Diagonal abajo derecha
+                    aristas[cont].append((nodo_diagonal[0] + 1, nodo_diagonal[1] + 1))
+
+                cont += 1                                                                               # Incrementamos el contador para pasar al siguiente nodo
+
+        else:
+            for nodo in nodos:
+
+                aristas.append([])  # Anydimos una lista vacia para cada nodo
+
+                if 0 <= nodo[0] - 1:                                                                    # Fila arriba
+                    aristas[cont].append((nodo[0] - 1, nodo[1]))
+                if nodo[0] + 1 < self.filas:                                                            # Fila abajo
+                    aristas[cont].append((nodo[0] + 1, nodo[1]))
+                if 0 <= nodo[1] - 1:                                                                    # Columna izquierda
+                    aristas[cont].append((nodo[0], nodo[1] - 1))
+                if nodo[1] + 1 < self.columnas:                                                         # Columna derecha
+                    aristas[cont].append((nodo[0], nodo[1] + 1))
+
+                cont += 1                                                                               # Incrementamos el contador para pasar al siguiente nodo
+
+
+        return aristas # Devolvemos las aristas para cada nodo
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
