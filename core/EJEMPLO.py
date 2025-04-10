9d53fa5e404e51014c4aad31c9bf84df84216927
Sergio Azorín Julián <saj13@alu.ua.es>
2025-04-10 16:50:48 +0200

    .

--- core/EJEMPLO.py
+++ core/EJEMPLO.py
@@ -5,18 +5,12 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token, ge
 import requests
 URL='http://127.0.0.1:5000'
 TOKEN =None
-r=requests.get('http://123.0.0.1.5000/')
 
-def get_headers(auth_required=False):
-    headers = {"Content-Type": "application/json"}
-    if auth_required and TOKEN:
-        headers["Authorization"] = f"Bearer {TOKEN}"
-    return headers
 
 
 def iniciar_sesion():
-    usuario = input("Usuario: ")
-    contrasena = input("Contraseña: ")
+    usuario = str(input("Usuario: "))
+    contrasena = str(input("Contraseña: "))
     r=requests.get(f'{URL}/login?user=<usuario>&password=<contraseña>')
     print(r.status_code)
     print(r.text)
@@ -26,15 +20,13 @@ def iniciar_sesion():
 
 
 def registrarse():
-    usuario = input("Usuario nuevo: ")
-    contrasena = input("Contraseña: ")
-    r=requests.post(f"{URL}/signup?user=<usuario>&password=<contrasena>")
+    usuario = str(input("Usuario nuevo: "))
+    contrasena = str(input("Contraseña: "))
+    r=requests.post(f"{URL}/signup?user=<string:usuario>&password=<string:contrasena>")
     print(r.status_code)
     print(r.text)
 
-def users_list():
-    r = requests.get(f"{URL}/users", headers=get_headers(auth_required=True))
-    print("Respuesta:", r.status_code, r.json())
+
 
 
 
@@ -88,8 +80,7 @@ if __name__ == '__main__':
         if opcion == "2":
             registrarse()
 
-        if opcion == "3":
-            users_list()
+
 
 
         else:
--- core/api.py
+++ core/api.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
 import requests
 
-
+URL='http://127.0.0.1:5000'
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "contraseñajwt"
 jwt = JWTManager(app)
@@ -13,40 +13,25 @@ jwt = JWTManager(app)
 def mensaje():
     return 'API Funcionando'
 
-
-
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    return list(users.keys()), 200
-
-
-@app.route('/users/<string:id>', methods=['POST'])
-def add_user(id):
-    if id not in users:
-        users[id] = request.args.get('value','')
-        return f'Dato {id} añadido' ,200
-    else:
-        return f'El usuario ya existe' ,409
-
-
+@app.route('/')
 @app.route('/singup',methods=['POST'])
 def signup():
+
     user=request.args.get('user','')
     if user in users:
         return f'El usuario ya existe' ,409
     else:
         password=request.args.get('password','')
-        hashed=hashlib.sha256(password.encode()).hexdigest()
-        users[user]=hashed
+
+        users[user]=password
         return f'Usuario {user} registrado' ,200
 
 @app.route('/login', methods=['GET'])
 def login():
     user=request.args.get('user','')
     password=request.args.get('password','')
-    hashed=hashlib.sha256(password.encode()).hexdigest()
-    if user in users and users[user]==hashed:
+
+    if user in users and users[user]==password:
         return create_access_token(identity=user),200
     else:
         return f'Usuario o contraseña incorrectos',401
@@ -54,4 +39,4 @@ def login():
 if __name__ == '__main__':
      app.run(debug=True)
 
-users={}
+users = {}
