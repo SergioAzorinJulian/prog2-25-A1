01f4ac1fe4f0f177223aebcde5c59f8d50aa2ad8
Sergio Azorín Julián <saj13@alu.ua.es>
2025-04-10 16:27:40 +0200

    probando con la api

--- core/EJEMPLO.py
+++ core/EJEMPLO.py
@@ -1,22 +1,96 @@
-URL_BASE='http://127.0.0.1:5000'
-TOKEN =None
+import hashlib
 import requests
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
+import requests
+URL='http://127.0.0.1:5000'
+TOKEN =None
+r=requests.get('http://123.0.0.1.5000/')
+
+def get_headers(auth_required=False):
+    headers = {"Content-Type": "application/json"}
+    if auth_required and TOKEN:
+        headers["Authorization"] = f"Bearer {TOKEN}"
+    return headers
+
+
+def iniciar_sesion():
+    usuario = input("Usuario: ")
+    contrasena = input("Contraseña: ")
+    r=requests.get(f'{URL}/login?user=<usuario>&password=<contraseña>')
+    print(r.status_code)
+    print(r.text)
 
 
 
 
-def login():
-    global TOKEN
-    user = input("Usuario: ")
-    passwd = input("Contraseña: ")
-    r = requests.get(f"{URL_BASE}/login", params={"user": user, "passwd": passwd})
+
+def registrarse():
+    usuario = input("Usuario nuevo: ")
+    contrasena = input("Contraseña: ")
+    r=requests.post(f"{URL}/signup?user=<usuario>&password=<contrasena>")
+    print(r.status_code)
+    print(r.text)
+
+def users_list():
+    r = requests.get(f"{URL}/users", headers=get_headers(auth_required=True))
     print("Respuesta:", r.status_code, r.json())
-    if r.status_code == 200:
-        TOKEN = r.json()["access_token"]
 
 
-def signup():
-    user = input("Usuario nuevo: ")
-    passwd = input("Contraseña: ")
-    r = requests.post(f"{URL_BASE}/signup", json={"user": user, "passwd": passwd})
-    print("Respuesta:", r.status_code, r.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def menu():
+    print("1. Iniciar sesión")
+    print("2. Registrarse")
+    print("3. Listar Users")
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        menu()
+        opcion = input("Elige una opción: ")
+        if opcion == "0":
+            print("Adios......")
+            break
+
+        if opcion == "1":
+            iniciar_sesion()
+
+        if opcion == "2":
+            registrarse()
+
+        if opcion == "3":
+            users_list()
+
+
+        else:
+            print("Opción no válida.")
--- core/api.py
+++ core/api.py
@@ -1,17 +1,19 @@
+import hashlib
+import secrets
+
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
 import requests
 
 
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "contraseñajwt"
+jwt = JWTManager(app)
 @app.route('/')
 def mensaje():
     return 'API Funcionando'
 
-if __name__ == '__main__':
-     app.run(debug=True)
 
-users={}
 
 
 @app.route('/users', methods=['GET'])
@@ -28,3 +30,28 @@ def add_user(id):
         return f'El usuario ya existe' ,409
 
 
+@app.route('/singup',methods=['POST'])
+def signup():
+    user=request.args.get('user','')
+    if user in users:
+        return f'El usuario ya existe' ,409
+    else:
+        password=request.args.get('password','')
+        hashed=hashlib.sha256(password.encode()).hexdigest()
+        users[user]=hashed
+        return f'Usuario {user} registrado' ,200
+
+@app.route('/login', methods=['GET'])
+def login():
+    user=request.args.get('user','')
+    password=request.args.get('password','')
+    hashed=hashlib.sha256(password.encode()).hexdigest()
+    if user in users and users[user]==hashed:
+        return create_access_token(identity=user),200
+    else:
+        return f'Usuario o contraseña incorrectos',401
+
+if __name__ == '__main__':
+     app.run(debug=True)
+
+users={}
